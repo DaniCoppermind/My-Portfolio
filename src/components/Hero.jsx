df9fbd4421b57be4b50f00d82facbaf05d5df712
Daniel García <workspacedaniel2121@gmail.com>
2024-08-29 13:54:09 -0400

    colors to logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,16 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[#f12711]' />
+          <div className='w-1 sm:h-80 h-40 orange-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I&apos;m <span className='text-[#915eff]'>Daniel</span>
+            Hi, I&apos;m{' '}
+            <span className='bg-gradient-to-l from-red-500 to-orange-500 text-transparent bg-clip-text'>
+              Daniel
+            </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Lorem Ipsum is simply dummy text <br className='sm:block hidden' />
